a368e17dfccac211de6520bfb38c9480eb037fe8
Martin Vaculik <mvaculik4@gmail.com>
2024-06-09 17:19:17 +0200

    just setup

---
+++ mainq.py
@@ -0,0 +1,147 @@
+import pygame as pg 
+from random import randrange
+from random import randint
+
+Window_Height = 1000
+Window_Width = 1000
+screen = pg.display.set_mode((Window_Width, Window_Height))
+Tile_Size = 100
+clock = pg.time.Clock()
+random_range = (Tile_Size, (Window_Width // Tile_Size) - Tile_Size)
+get_random_position = lambda: [randrange(1, (Window_Width // Tile_Size)) * Tile_Size, randrange(1, (Window_Height // Tile_Size)) * Tile_Size]
+
+class Snake:
+    def __init__(self):
+        self.length = 1
+        self.head = []
+        self.positions = []
+        self.direction = 0
+        self.lastpositionbuff = []
+
+    def random_head_position(self):
+        self.head.append(((randrange(100, Window_Width, Tile_Size), randrange(100, Window_Height, Tile_Size))))
+        self.positions = self.head
+    def move(self):
+        snake_len = len(self.positions) - 1
+       
+        self.lastpositionbuff = self.positions[snake_len]
+        del self.positions[snake_len] 
+
+        if snake_len == 0:
+            if self.direction == 0:
+                self.positions.insert(0, (self.lastpositionbuff[0], self.lastpositionbuff[1] - Tile_Size))
+            if self.direction == 1:
+                self.positions.insert(0, (self.lastpositionbuff[0] + Tile_Size, self.lastpositionbuff[1]))
+            if self.direction == 2:
+                self.positions.insert(0, (self.lastpositionbuff[0], self.lastpositionbuff[1] + Tile_Size))
+            if self.direction == 3:
+                self.positions.insert(0, (self.lastpositionbuff[0] - Tile_Size, self.lastpositionbuff[1]))
+        else:
+            if self.direction == 0:
+                self.positions.insert(0, (self.positions[0][0], self.positions[0][1] - Tile_Size))
+            if self.direction == 1:
+                self.positions.insert(0, (self.positions[0][0] + Tile_Size, self.positions[0][1]))
+            if self.direction == 2:
+                self.positions.insert(0, (self.positions[0][0], self.positions[0][1] + Tile_Size))
+            if self.direction == 3:
+                self.positions.insert(0, (self.positions[0][0] - Tile_Size, self.positions[0][1]))
+        
+        
+
+
+class Food: 
+    def __init__(self):
+        self.position = []
+
+    def randomize_position(self):
+        self.position.append(((randrange(0, Window_Width, Tile_Size), randrange(0, Window_Height, Tile_Size))))
+        for i in self.position:
+            if i == 0 or i == 25:
+                i = 50;
+
+    def draw(self):
+        pg.draw.rect(screen, 'red', (self.position[0], self.position[1], Tile_Size, Tile_Size))
+
+
+
+
+def check_collision(snake, food):
+    if snake.positions[0] == food.position[0]:
+        snake.length += 1
+        snake.positions.append(snake.lastpositionbuff)
+        food.position = []
+        food.randomize_position()
+    if snake.positions[0][0] == Window_Width or snake.positions[0][0] == 0:
+        print(snake.positions[0][0])
+        print('Game Over')
+        pg.quit()
+        quit()
+    if snake.positions[0][1] == Window_Height or snake.positions[0][1] == 0:
+        print(snake.positions[0][1])
+        print('Game Over')
+        pg.quit()
+        quit()
+    if snake.length > 1:
+        for i in snake.positions[1:]:
+            if snake.positions[0] == i:
+                print('Game Over')
+                pg.quit()
+                quit()
+        
+    
+
+
+
+
+
+
+
+def main():
+    snake = Snake()
+    Snake.random_head_position(snake)
+    food = Food()
+    Food.randomize_position(food)
+    while True: 
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_UP:
+                    snake.direction = 0
+                if event.key == pg.K_RIGHT:
+                    snake.direction = 1
+                if event.key == pg.K_DOWN:
+                    snake.direction = 2
+                if event.key == pg.K_LEFT:
+                    snake.direction = 3
+                if event.key == pg.K_w:
+                    snake.direction = 0
+                if event.key == pg.K_d:
+                    snake.direction = 1
+                if event.key == pg.K_s:
+                    snake.direction = 2
+                if event.key == pg.K_a:
+                    snake.direction = 3
+        screen.fill((0, 0, 0))
+        
+        counter_head = 0
+        
+        check_collision(snake, food)
+        snake.move()
+        [pg.draw.rect(screen, 'red', (x, y, Tile_Size, Tile_Size)) for x, y in food.position]
+        for x, y in snake.positions:
+            print(snake.positions)
+            if counter_head == 0:
+                [pg.draw.rect(screen, 'green', (x, y, Tile_Size, Tile_Size))]
+                counter_head += 1
+            else:
+                [pg.draw.rect(screen, 'blue', (x, y, Tile_Size, Tile_Size))]
+
+
+        
+        pg.display.flip()
+        clock.tick(4)
+
+if __name__ == '__main__':
+    main()
--- qlearning.py
+++ qlearning.py
@@ -1,22 +1,39 @@
 import sys 
 import numpy as np
 from collections import defaultdict
+from PIL import Image
+import cv2
+import matplotlib.pyplot as plt
+from matplotlib import style
 import pickle
 from time import sleep, time
 import main
+import time 
 
-if sys.argv[1] == "p":
-    mode = 0
-if sys.argv[1] == "t":
-    mode = 1
+
+
+
 
 rewardFood = 500
 rewardDeath = -50000
-alpha = 0.1
-alpha_decay = 0.999
+epsilon_decay = 0.9998
+epsilion = 0.9
 gamma = 0.9
+size = 10
+episodes = 25000
+show_every = 1000
+startqtable = None # or filename
 
-if mode == 0:
-    pass
-if mode == 1:
-    pass
+learning_rate = 0.1
+discount = 0.95
+
+player_head_n = 1
+player_body_n = 2
+food_n = 3
+
+d = {1: (0, 255, 0), 2: (255, 0 ,0), 3: (0, 0, 255)} # bgr colors
+
+if startqtable == None:
+    qtable = defaultdict(lambda: [np.random.uniform(-5, 0) for i in range(4)])
+
+
